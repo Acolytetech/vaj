8dd7c960338f73da82df488d921937dc6459259c
Acolytetech <slawaniya03@gmail.com>
2024-06-03 16:56:06 +0530

    frontned

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
     parser: 'babel-eslint',
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    plugins: ['react', 'react-hooks'],
-    rules: {
-      // Add specific rules here if needed
-    },
+    // Other ESLint configurations...
   };
   
